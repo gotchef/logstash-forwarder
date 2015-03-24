78b3046469292efb69925b4e7eca41855678e9b2
Franklin Wise <franklin@krave.io>
2015-03-23 20:21:33 -0700

    refactor config root

--- attributes/default.rb
+++ attributes/default.rb
@@ -8,16 +8,8 @@ name = 'logstash-forwarder'
 default[:logstash_forwarder][:build][:repository] = "https://github.com/elastic/logstash-forwarder.git"
 default[:logstash_forwarder][:build][:revision] = "master"
 default[:logstash_forwarder][:build][:command] = "" # make || go install (default)
-default[:logstash_forwarder][:build][:go_main_dir] = nil #root dir "logstash_forwarder"
-
-default[:logstash_forwarder][:service][:install_root] = "/opt"
-default[:logstash_forwarder][:service][:config_root] = "/etc"
-default[:logstash_forwarder][:service][:log_root] = "/var/log"
-default[:logstash_forwarder][:service][:timeout] = 60
 default[:logstash_forwarder][:service][:run_file] = 'logstash-forwarder'
 default[:logstash_forwarder][:service][:name] = name
-default[:logstash_forwarder][:service][:user] = name
-default[:logstash_forwarder][:service][:group] = name
 
 default[:github][:deploy_key] = "passed in"
 
--- recipes/service.rb
+++ recipes/service.rb
@@ -4,6 +4,10 @@
 
 include_recipe 'go-service'
 
+service = node[:logstash_forwarder][:service]
+
 go_service_service do
-	service node[:logstash_forwarder][:service]
+	service_name	service[:name]
+	run_file		service[:run_file]
+	run_args		"-config #{Chef::GoService.config_root(service)}/#{service[:name]}/"
 end
