e523630b0395d2ea8c6cc161c31e59eb46126b5b
Franklin Wise <franklin@krave.io>
2015-04-01 14:01:43 -0700

    clean up

--- recipes/configure.rb
+++ recipes/configure.rb
@@ -5,28 +5,16 @@
 service = node[:logstash_forwarder][:service]
 service_name = service[:name]
 
-user = service[:user]
-group = service[:group]
-
 runit_service service_name do
 	action :nothing # only define so that it can be restarted if the config changed
 end
 
-config_root = "#{service[:config_root]}/#{service_name}"
-
-directory "#{config_root}" do
-	group group
-	owner user
-	mode '0775'
-	action :create
-end
-
-template "#{config_root}/config.json" do
+template "#{Chef::GoService.config_dir(service)}/config.json" do
 	source 'config.json.erb'
 	cookbook 'logstash-forwarder'
 	mode '0664'
-	owner user
-	group group
+	owner Chef::GoService.user(service)
+	group Chef::GoService.group(service)
 	variables()
 	notifies :restart, "runit_service[#{service_name}]"
 end
